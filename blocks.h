4e06b91fff22997f783d35b69891c4344db92c09
krypek <krypek@tuta.io>
2023-05-24 21:26:56 +0200

    Change bar scripts dir

--- blocks.h
+++ blocks.h
@@ -1,25 +1,25 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰍛", "$HOME/.config/dotfiles/scripts/bar/mem.sh",	2,		0},
+	{"󰍛", "$HOME/.config/scripts/bar/mem.sh",	2,		0},
 
-	{"", "$HOME/.config/dotfiles/scripts/bar/swap.sh",	2,		0},
+	{"", "$HOME/.config/scripts/bar/swap.sh",	2,		0},
 
-    {"", "$HOME/.config/dotfiles/scripts/bar/cpu.sh",  2, 0},
+    {"", "$HOME/.config/scripts/bar/cpu.sh",  2, 0},
 
-    {"", "$HOME/.config/dotfiles/scripts/bar/cputemp.sh",    2,      0},
+    {"", "$HOME/.config/scripts/bar/cputemp.sh",    2,      0},
 
-    {"", "$HOME/.config/dotfiles/scripts/bar/screentemp-gammastep.sh",  15,     0},
+    {"", "$HOME/.config/scripts/bar/screentemp-gammastep.sh",  15,     0},
 
-    {"", "$HOME/.config/dotfiles/scripts/bar/network-traffic.sh",  5,      0},
+    {"", "$HOME/.config/scripts/bar/network-traffic.sh",  5,      0},
 
-    {"", "$HOME/.config/dotfiles/scripts/bar/updates.sh", 60, 0},
+    {"", "$HOME/.config/scripts/bar/updates.sh", 60, 0},
 
-    {" ", "$HOME/.config/dotfiles/scripts/bar/uptime.sh", 60, 0},
+    {" ", "$HOME/.config/scripts/bar/uptime.sh", 60, 0},
 
     {" ", "cat /tmp/keyboard_layout",                                  1,      0},
 
-	{"", "$HOME/.config/dotfiles/scripts/bar/date.sh",		10,		0},
+	{"", "$HOME/.config/scripts/bar/date.sh",		10,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
